e27d0740b5c93205a2719f8d468e220ec2ee6767
Elijah Prince <elijahprince73@gmail.com>
2019-05-14 23:22:01 -0700

    Getting started with react-graphql

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://user:red123@ds157136.mlab.com:57136/lyrical-graphql';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
